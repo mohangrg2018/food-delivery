ee4735d22e2b2d5a1ce70f81d042224df7122d53
mohangrg2018 <mohangrg2030@gmail.com>
2025-09-15 13:18:53 +0545

    feat: implement Hero component with background image and text content

--- client/src/components/sections/Hero.jsx
+++ client/src/components/sections/Hero.jsx
@@ -1,5 +1,21 @@
+import { Button } from "../ui/button";
+
 const Hero = () => {
-  return <section>H</section>;
+  return (
+    <section className="container__width mt-2">
+      <div className="hero_background h-[60vh] lg:h-[80vh] flex flex-col gap-4 items-start justify-center px-8 text-white rounded-md">
+        <h1 className="text-4xl font-bold">Order your favourite food here</h1>
+        <p className="w-[70vw]">
+          A fast, user-friendly platform that lets customers browse restaurants,
+          discover menus, place orders online, and get fresh food delivered to
+          their doorsteps.
+        </p>
+        <Button variant="outline" className="text-black">
+          View Menu
+        </Button>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
